a95b0d2b58f6f586cd32dff943178579a3b1f762
Daniela Mock Zubia <mockzubia@gmail.com>
2021-08-10 16:39:43 +0200

    functionality of wiki

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,11 +8,11 @@
 # require 'faker'
 
 
-# 10.times do
-#   @article = Article.new(
-#     title:    Faker::Book.title,
-#     content:  Faker::Lorem.characters
-#   )
-#   @article.save!
-# end
-# puts 'Finished!'
+10.times do
+  @article = Article.new(
+    title:    Faker::Book.title,
+    content:  Faker::Lorem.sentence
+  )
+  @article.save!
+end
+puts 'Finished!'
